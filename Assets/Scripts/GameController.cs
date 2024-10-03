6f5c2c6cc7be0461cc9c0d4ea459d076ca8106c1
Gabriel Carvalho <gabrielhenriquerc11@gmail.com>
2024-10-03 17:04:43 -0300

    feat: game over

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     public int totalScore;
     public TextMeshProUGUI scoreText;
-
     public static GameController instance;
+    public GameObject gameOver;
 
     // Start is called before the first frame update
     void Start()
@@ -26,5 +27,15 @@ public class GameController : MonoBehaviour
     public void UpdateScoreText()
     {
         scoreText.text = totalScore.ToString();
+    }
+
+    public void ShowGameOver()
+    {
+        gameOver.SetActive(true);
+    }
+
+    public void RestartGame(string lvlName)
+    {
+        SceneManager.LoadScene(lvlName);
     }
 }
